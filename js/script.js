6252a7386a613cb11c64afbd8045b5d05d167086
ipolekhin <ipolekhin@yadnex.ru>
2019-12-27 10:50:34 +0300

    Добавление Карусели owl 2

--- js/script.js
+++ js/script.js
@@ -9,4 +9,30 @@ $(function() {
       console.log('карусель1');
     }
   }
+
+  var owl = $('.owl-carousel');
+  owl.owlCarousel({
+    autoWidth: true,
+    center: true,
+    items: 3,
+    loop: true,
+    nav: false,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:true
+      },
+      577: {
+        autoplay:false,
+      },
+      991: {
+        items: 2,
+      }
+    }
+  });
+
 });
+
